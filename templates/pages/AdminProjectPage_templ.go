c969d2e1fe5ed39962b5671e848efc300c2b4870
TS22082 <ts22082@gmail.com>
2025-09-11 12:13:30 -0700

    fix nav highlighting

--- templates/pages/AdminProjectPage_templ.go
+++ templates/pages/AdminProjectPage_templ.go
@@ -36,7 +36,7 @@ func AdminProjectPage(apiKeys []project_types.ApiKey) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = components.UserNav("Oggee", "dashboard").Render(ctx, templ_7745c5c3_Buffer)
+		templ_7745c5c3_Err = components.UserNav("Oggee", "projects").Render(ctx, templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
